02317410daa30de237bc815d190d1f814a5ff8cd
nova <novaex@tuta.io>
2025-12-04 20:48:02 -0600

    undo font/icon changes

--- userconfig.js
+++ userconfig.js
@@ -36,8 +36,8 @@ const default_configuration = {
     "s": "search-bar",
   },
   disabled: [],
-  localIcons: false,
-  localFonts: false,
+  localIcons: true,
+  localFonts: true,
   fastlink: "https://www.perplexity.ai",
   openLastVisitedTab: true,
   tabs: [
